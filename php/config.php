67a5c2f55fb4eb483bc5111bd132179f5eb815a8
Eric Odom <eric@homecareintel.com>
2015-02-15 14:56:19 -0500

    initial commit

---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php 
+/* config.php */
+
+/* OVNIResponsive Template */
+/* autor: entiri s.r.o. */
+
+/* Enter your e-mail address */
+/* All messages from the website will be sent to this e-mail address. */
+
+$email_address = 'eric@homecareintel.com';
+                                             
+/* Location: php/config.php */
+?>
